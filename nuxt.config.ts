0edad24c70cd9156badd164ad891cfc3fc6bd4aa
Rafael Silva <rafaelsilvar74@gmail.com>
2026-08-08 15:19:56 -0300

    feat: Update logo and manifest files for Magia Cristais
    - Replaced logo image with a new version.
    - Updated site.webmanifest to use new icon paths and removed unnecessary entries.
    - Deleted old web app manifest images that are no longer in use.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'shortcut icon', href: '/favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'manifest', href: '/site.webmanifest' },
