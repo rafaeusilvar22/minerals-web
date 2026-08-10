48a2cd18391e99e892d7db9088cebbc1d460ca4b
Rafael Silva <rafaelsilvar74@gmail.com>
2026-08-10 13:17:12 -0300

    feat: adiciona atributo de linguagem 'pt-BR' no HTML da configuração do Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'pt-BR',
+      },
       link: [
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
